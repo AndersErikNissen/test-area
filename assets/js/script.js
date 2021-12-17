90c7d1336db473c8f3d32288ace262eada95a0a9
Anders Erik Nissen <73883535+AndersErikNissen@users.noreply.github.com>
2021-12-17 22:52:33 +0100

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//JS by Anders Erik Nissen
+//UCN MMDA0920
+
+
+console.log("        /\\       _______   ___    __ ")
+console.log("       /  \\     |   ____| |   \\  |  |")
+console.log("      / /\\ \\    |  |____  |    \\ |  |")
+console.log("     / /__\\ \\   |   ____| |  |\\ \\|  |")
+console.log("    / /____\\ \\  |  |____  |  | \\    |")
+console.log("   /_/      \\_\\ |_______| |__|  \\___|")
+
+
+
+ 
